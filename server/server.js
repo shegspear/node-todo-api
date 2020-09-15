ddcbd65d9be82f8c0ce0f75a079f40a4988d6f4d
shegspear <57143805+shegspear@users.noreply.github.com>
2020-09-15 18:53:52 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 // THIRD PARTY LIBRZRY
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // LOCAL LIBRARY
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-const { ObjectID } = require('mongodb');
+const { ObjectID, BSONType } = require('mongodb');
+const todo = require('./models/todo');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -78,6 +80,33 @@ app.delete('/todos/:id', (req, res) => {
         .catch((e) => {
             res.status(400).send('Sorry an error occured :', e);
         });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('Could not find your update');
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send('Sorry an error occured with your update');
+    });
+
 });
 
 app.listen(port, () => {
