92e931c3ae71090290a08ec7bf46b057514d8621
shegspear <57143805+shegspear@users.noreply.github.com>
2020-09-20 14:55:41 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -110,6 +110,7 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -127,6 +128,21 @@ app.post('/users', (req, res) => {
 // example of private route
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
